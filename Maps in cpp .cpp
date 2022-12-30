9fee8ded8885766ff352d86e07a795c94ed49678
guna17215 <109857836+guna17215@users.noreply.github.com>
2022-12-30 10:16:51 +0530

    Update Maps in cpp .cpp

--- Maps in cpp .cpp
+++ Maps in cpp .cpp
@@ -1,3 +1,5 @@
+// PART - I
+
 #include <iostream>
 #include <vector>
 #include <iterator>
@@ -31,5 +33,60 @@ int main(){
     cout << "SIZE OF THE MAP IS: " << mp.max_size() << endl;  //TO FIND THE MAXIMUM SIZE OF THE MAP.
     cout << "IS EMPTY: " << mp.empty() << endl;  // CHECK WHETHER MAP IS EMPTY OR NOT. IF EMPTY RETURNS 1 ELSE O.
     cout << "CLEAR FUNCTION: " << mp.clear();  // CLEAR FUCTION CLEARS ALL ELEMENTS IN THE MAP.
+    return 0;
+}
+
+// PART - II
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <map>
+using namespace std;
+int main(){
+    map <int, int> mp1;
+    
+    for(int i = 1; i <= 5; i++){
+        mp1.insert({i, i*7});
+    }
+    
+    // map <int, int> mp2;
+    // mp2 = mp1; // 1. COPYING THE MAP.
+    map <int, int> mp2(mp1.begin(), mp1.end()); // 2. COPYING THE MP1 INTO MP2.
+    
+    // ACCESSING THE COPIED MAP.
+    cout << "THE ELEMENTS IN MP2 ARE: \n";
+    for(auto it = mp2.begin(); it != mp2.end(); it++){
+        cout << it -> first << " " << it -> second << endl;
+    }
+    
+    mp1[6] = 6*7; // AFTER COPYING THE CHANGES MADE IN mp1 DOESN'T REFLECT IN mp2.
+    mp2.insert({7, 7*7}); // AFTER COPYING THE CHANGES MADE IN mp2 DOESN'T REFLECT IN mp1.
+    
+    cout << "THE ELEMENTS IN MP1 AFTER INSERTING NEW ELEMENT ARE: \n";
+    for(auto it = mp1.begin(); it != mp1.end(); it++){
+        cout << it -> first << " " << it -> second << endl;
+    }
+    
+    cout << "THE ELEMENTS IN MP2 AFTER INSERTING NEW ELEMENT INTO MP1 AND MP2 ARE: \n";
+    for(auto it = mp2.begin(); it != mp2.end(); it++){
+        cout << it -> first << " " << it -> second << endl;
+    }
+    
+    // CONCLUSION : THOUGH MP2 IS COPY OF MP1 THE CHANGES MADE IN MP1 DOESN'T REFLECT IN MP2 AND VIS A VIS.
+    
+    // USE OF ERASE FUNCTION.
+    mp1.erase(mp1.begin(), mp1.find(3)); // LAST ELEMET IS NOT INCLUDED i.e [0, 3).
+    cout << "THE ELEMENTS IN MP1 AFTER ERASING ARE: \n";
+    for(auto it = mp1.begin(); it != mp1.end(); it++){
+        cout << it -> first << " " << it -> second << endl;
+    }
+    
+    mp2.erase(7); // REMOVE ALL ELEMETS WITH KEY 7.
+    cout << "THE ELEMENTS IN MP2 AFTER ERASING ARE: \n";
+    for(auto it = mp2.begin(); it != mp2.end(); it++){
+        cout << it -> first << " " << it -> second << endl;
+    }
+    
+    // CONCLUSION: erase WHILE TAKING TWO ARGUMENTS TAKES (iterator, position). WHILE TAKING ONE ARGUMENT TAKES ONLY KEYVAALUE.
     return 0;
 }
