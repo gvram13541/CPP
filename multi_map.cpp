d14ab2cbaba3c6013367ff8bcb48e5fc93392093
guna17215 <109857836+guna17215@users.noreply.github.com>
2022-12-30 16:02:50 +0530

    Update multi_map.cpp

--- multi_map.cpp
+++ multi_map.cpp
@@ -1,1 +1,4 @@
-
+// Multimap is similar to a map with the addition that multiple elements can have the same keys. 
+// Also, it is NOT required that the key-value and mapped value pair have to be unique in this case.
+// One important thing to note about multimap is that multimap keeps all the keys in sorted order always. 
+// These properties of multimap make it very much useful in competitive programming.
