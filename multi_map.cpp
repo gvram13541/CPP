67fb6de831b7e0f4cfe85ebda93a2769c08dc085
guna17215 <109857836+guna17215@users.noreply.github.com>
2023-01-02 12:02:47 +0530

    Update multi_map.cpp

--- multi_map.cpp
+++ multi_map.cpp
@@ -2,3 +2,5 @@
 // Also, it is NOT required that the key-value and mapped value pair have to be unique in this case.
 // One important thing to note about multimap is that multimap keeps all the keys in sorted order always. 
 // These properties of multimap make it very much useful in competitive programming.
+
+// UNOREDERD_MULTIMAP BEAHVES SAME AS THAT OF UNORDERD_MAP IN ADDITON IT CAN STORE MULTI VALUES I.E REPETIONS ARE ALLOWED.
