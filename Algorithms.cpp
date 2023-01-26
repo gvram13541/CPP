9e70e4bc1e3b77c573694c016e6db36c8bfb0ce6
guna17215 <109857836+guna17215@users.noreply.github.com>
2023-01-26 10:26:51 +0530

    Update Algorithms.cpp

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -1,3 +1,5 @@
+// <============ TWO POINTER ALGORITHM ==============>
+
 //1. An alogorithm to remove duplicate elements in the sorted array.
 
 int remove_duplicate(int a[],int n){
@@ -9,6 +11,39 @@ int remove_duplicate(int a[],int n){
             }
         }
         return i+1;
+}
+
+// 2. An algorithm to find union of two sorted arrays.
+
+vector < int > FindUnion(int arr1[], int arr2[], int n, int m) {
+  int i = 0, j = 0; // pointers
+  vector < int > Union; // Uninon vector
+  while (i < n && j < m) {
+    if (arr1[i] <= arr2[j]) // Case 1 and 2
+    {
+      if (Union.size() == 0 || Union.back() != arr1[i])
+        Union.push_back(arr1[i]);
+      i++;
+    } else // case 3
+    {
+      if (Union.size() == 0 || Union.back() != arr2[j])
+        Union.push_back(arr2[j]);
+      j++;
+    }
+  }
+  while (i < n) // IF any element left in arr1
+  {
+    if (Union.back() != arr1[i])
+      Union.push_back(arr1[i]);
+    i++;
+  }
+  while (j < m) // If any elements left in arr2
+  {
+    if (Union.back() != arr2[j])
+      Union.push_back(arr2[j]);
+    j++;
+  }
+  return Union;
 }
 
 /* This is an implementation of the Two Pointers algorithm for removing duplicates from an array. The algorithm uses two pointers, one (i) to iterate 
