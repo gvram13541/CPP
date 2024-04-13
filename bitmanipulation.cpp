2af52b6af03bed1acf28c50b4ff69e836666a727
P Gunavardhan Reddy <109857836+gvram13541@users.noreply.github.com>
2024-04-13 10:15:15 +0530

    Create bitmanipulation.cpp

---
+++ bitmanipulation.cpp
@@ -0,0 +1,16 @@
+// The bitset header file provides us many built in functions to deal with bits.
+#include <bitset>
+
+using namespace std;
+
+// bitset declatration and initilization
+
+void decandinit(){
+  //bitset<size> variable_name;
+  // https://www.geeksforgeeks.org/cpp-bitset-and-its-application/
+}
+
+int main(){
+  
+  return 0;
+}
